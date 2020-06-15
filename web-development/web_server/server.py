7e1d9a1dc51ad6b9f02f91b80c43677e00cbe23c
Alex <alexstocki22@gmail.com>
2020-06-15 16:19:59 -0300

    mejoras contact

--- web-development/web_server/server.py
+++ web-development/web_server/server.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
 
-#@app.route('/<username>/<int:post_id>')
-#def hello_world(username=None, post_id=None):
-#   return render_template('index.html', name=username, post_id=post_id)
-
-#@app.route('/<username>')
-#def hello_world(username=None):
-#   return render_template('index.html', name=username)
-
-# probando la reutilizacion de acuerdo al valor ingresado
 @app.route('/')
 def main():
    return render_template('index.html')
 
+def write_to_csv(data):
+   with open('database.csv', newline='', mode='a') as database:
+      writer = csv.writer(database, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+      writer.writerow([data["email"], data["subject"], data["message"]])
+
 @app.route('/submit_form', methods=['GET', 'POST'])
 def submit_form():
    if request.method == 'POST':
-      data = request.form.to_dict()
-      with open('database.txt', 'a+') as database:
-         database.write(f'From: {data["email"]}; Subject: {data["subject"]}; Message: {data["message"]}\n')
-      return redirect('/contact_done.html')
+      try:
+         data = request.form.to_dict()
+         write_to_csv(data)
+         return redirect('/contact_done.html')
+      except:
+         return 'Check the values and try again.'
    else:
-      return 'Algo salio mal, pa. Proba de nuevo.'
+      return 'Something went wrong, try again.'
 
 @app.route('/<section>')
 def page_html(section):
